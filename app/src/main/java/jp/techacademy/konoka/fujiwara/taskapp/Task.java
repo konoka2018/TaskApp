fe31dd4a7d02238fe607aaec00aa591e90a5c4bd
konoka <ririmaru2003@gmail.com>
2019-01-07 22:07:43 +0900

    first2

---
+++ app/src/main/java/jp/techacademy/konoka/fujiwara/taskapp/Task.java
@@ -0,0 +1,54 @@
+package jp.techacademy.konoka.fujiwara.taskapp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+//プライマリーキーとは主キーとも呼ばれ、データーベースの一つのテーブルの中でデータを唯一的に確かめるための値
+
+
+//Serializableとはデータを丸ごとファイルに保存したり、
+//TaskAppでいうと別のActivityに渡すことができるようにすることです。
+public class Task extends RealmObject implements Serializable {
+    private String title; // タイトル
+    private String contents; // 内容
+    private Date date; // 日時
+
+    // id をプライマリーキーとして設定
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+        toString()
+    }
+}
---
+++ app/src/main/java/jp/techacademy/konoka/fujiwara/taskapp/TaskApp.java
@@ -0,0 +1,14 @@
+package jp.techacademy.konoka.fujiwara.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
+
